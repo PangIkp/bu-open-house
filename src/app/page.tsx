9006168bbd18564c2b8910e8dc08de6e4140d043
Rangtude <panggylovely@gmail.com>
2024-09-14 23:22:43 +0700

    change word

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         
         <div className="container mx-auto px-4 py-8 pt-40">
         
-          <h1 >Welcome to the Home Page</h1>
+          <h1>การแต่งกาย</h1>
         
         </div>
       </main>
