24c87169ff92bf319f04d8edc6b297ab291a9d18
Hunxoza <hunsa.dev@gmail.com>
2024-09-21 13:05:33 +0700

    refactor: add type to Button component props

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,56 @@
 'use client';
 import React, { forwardRef } from 'react';
 
-export const Button = forwardRef<
-    HTMLButtonElement,
-    React.ButtonHTMLAttributes<HTMLButtonElement>
->(({ children, ...props }, ref) => {
-    return (
-        <button
-            ref={ref}
-            {...props}
-            className='w-full p-3 bg-gradient-to-r from-[#666699] to-white hover:from-[#BEBEBE] hover:to-[#666699] rounded-full items-center justify-center'
-        >
-            {children}
-        </button>
-    );
-});
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'primary' | 'ghost' | 'plain'; // Define your variants here
+    color?: string; // New prop for custom color
+    size?: 'sm' | 'md' | 'lg'; // Add size prop
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ children, variant = 'primary', color, size = 'md', ...props }, ref) => {
+        // Define base styles for all button variants
+        const baseStyles =
+            'p-2 inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50';
+
+        let variantStyles = '';
+
+        switch (variant) {
+            case 'primary':
+                variantStyles = `${baseStyles} bg-gradient-to-r from-[#666699] to-white hover:from-[#BEBEBE] hover:to-[#666699]`;
+                break;
+            case 'ghost':
+                variantStyles = `${baseStyles} focus-visible:ring-none`;
+                break;
+            case 'plain':
+                variantStyles = `${color} text-white h-20 w-full transition duration-300 ease-in-out hover:scale-110 hover:rotate-2`; // Use the custom color
+                break;
+        }
+
+        // Add size classes using Tailwind CSS
+        let sizeClasses = '';
+        switch (size) {
+            case 'sm':
+                sizeClasses = 'py-1 px-2 text-xs';
+                break;
+            case 'lg':
+                sizeClasses = 'py-3 px-4 text-lg';
+                break;
+            default: // md
+                sizeClasses = 'py-2 px-3 text-sm';
+                break;
+        }
+
+        return (
+            <button
+                ref={ref}
+                {...props}
+                className={`${baseStyles} ${variantStyles} ${sizeClasses}`}
+            >
+                {children}
+            </button>
+        );
+    }
+);
 
 Button.displayName = 'Button';
